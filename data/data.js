0b03044621606794f42fff473665b6b6d47837c2
Kaya Winn <mail@psyreal.net>
2017-10-21 19:30:33 -0700

    adds session state variables

---
+++ data/data.js
@@ -0,0 +1,17 @@
+module.exports = [
+                {Input: "Cost Management Plan",            ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Scope Baseline",                  ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Activity Cost Estimates",         ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Basis of Estimates",              ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Project Schedule",                ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Resource Calendars",              ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Risk Register",                   ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Input: "Agreements",                      ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Tool: "Cost Aggregation",                 ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Tool: "Reserve Analysis",                 ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Tool: "Expert Judgement",                 ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Tool: "Historical Relationships",         ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Tool: "Funding Limit Reconcillation",     ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Output: "Cost Baseline",                  ProcessGroup: "Determine Budget",      Phase: "Planning "},
+                {Output: "Project Funding Requirements",   ProcessGroup: "Determine Budget",      Phase: "Planning "}
+          ];
--- src/handler.js
+++ src/handler.js
@@ -1,68 +1,53 @@
 'use strict';
 
 var Alexa = require('alexa-sdk');
-// const questions = require('./question');
 
 // const ANSWER_COUNT = 2;
 // const GAME_LENGTH = 1;
+
+var counter = 0;
+
+var states = {
+    START: "_START",
+    QUIZ: "_QUIZ",
+    FACTS:"_FACTS",
+    INPUTS: "_INPUTS",
+    OUTPUS: "_OUTPUTS"
+};
+
 var APP_ID = "amzn1.ask.skill.8bc9a7a3-6c83-4d35-a763-46bd5d102139";
 
-
 const FACTS = require('data/facts.json');
+const INPUTS = require('data/inputs');
+const GOODBYE = require('data/goodbye.json');
+// const DATA = require('data/data.json');
 const SKILL_NAME = 'PMP Facts';
 const GET_FACT_MESSAGE= "Here's your fact: ";
-const HELP_MESSAGE= 'You can say tell me a PMP fact, or, you can say quiz game, or, you can say exit... Which would you like?';
+const HELP_MESSAGE= 'You can say tell me a PM P fact, or, you can say quiz game, or, you can say exit... Which would you like?';
 const HELP_REPROMPT='What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
-
-// //Adding greater variety to endSession
-// var GOODBYE = [
-//     "OK, later Dude!",
-//     "OK, bye for now.",
-//     "OK, nice chatting - see you later.",
-//     "You bet! See you later.",
-//     "OK, no worries. See you later.",
-//     "OK, no problem. See you later.",
-//     "OK, see you later.",
-//     "Sure thing. See you later."
-// ];
-
-var data = [
-                {Input: "Cost Management Plan",            ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Scope Baseline",                  ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Activity Cost Estimates",         ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Basis of Estimates",              ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Project Schedule",                ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Resource Calendars",              ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Risk Register",                   ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Input: "Agreements",                      ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Tool: "Cost Aggregation",                 ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Tool: "Reserve Analysis",                 ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Tool: "Expert Judgement",                 ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Tool: "Historical Relationships",         ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Tool: "Funding Limit Reconcillation",     ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Output: "Cost Baseline",                  ProcessGroup: "Determine Budget",      Phase:'Planning'},
-                {Output: "Project Funding Requirements",   ProcessGroup: "Determine Budget",      Phase:'Planning'}
-          ];
+const quotes = require('data/data');
 
 
 // Skill Code --------------------------------------------------
 // Routes incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
+// etc.)
 
+console.info('Loading function');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context, callback);
     alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
-    callback(null, "success");
+    callback(null, "Success");
+    console.info('Handler ran');
 };
+
 var handlers = {
-    //This skill will receive a LaunchRequest when the user invokes the skill with the invocation name, but does not provide any command mapping to an intent. For example, open PMPnow
     'LaunchRequest': function () {
-      this.emit(":tell", "Welcome to PMPnow"); // Create speech output. This is what Alexa will speak back when the user says "Open PMPnow"
+      this.emit(":tell", "Welcome to PM P now");
     },
     'GetNewFactIntent': function () {
         this.emit('GetFact');
@@ -75,6 +60,7 @@ var handlers = {
             // Create speech output
             const speechOutput = GET_FACT_MESSAGE + randomFact;
             this.emit(':tell', speechOutput, SKILL_NAME, randomFact);
+            console.log('Getfact speach output runs');
     },
     'AMAZON.HelpIntent': function () {
        const speechOutput = HELP_MESSAGE;
